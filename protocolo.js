f70b9c192c948432edbee92a7eb0981f02b6d15f
Drbonis <drbonis@gmail.com>
2016-05-09 10:11:14 +0000

    primer borrador para validacion, sin ayudas

--- protocolo.js
+++ protocolo.js
@@ -48,12 +48,29 @@ hideAllCovariables = function () {
     });
 }
 
+hideElement = function(myElement) {
+    if($(myElement).hasClass("hidden")==false) {
+        $(myElement).toggleClass("hidden");
+        console.log("oculto desde hideElement");
+        console.log(myElement);
+    }
+}
+
+showElement = function(myElement) {
+    if($(myElement).hasClass("hidden")) {
+        $(myElement).toggleClass("hidden");
+        console.log("muestro desde showElement");
+        console.log(myElement);
+    }
+}
+
 showCovariable = function(targetRoot, numCov) {
     hideAllCovariables();
     $("#"+targetRoot+"_1_"+numCov.toString()).toggleClass("hidden");
     $("#"+targetRoot+"_2_"+numCov.toString()).toggleClass("hidden");
     $("#"+targetRoot+"_3_"+numCov.toString()).toggleClass("hidden");
     $("#"+targetRoot+"_4_"+numCov.toString()).toggleClass("hidden");
+    $("#"+targetRoot+"_5_"+numCov.toString()).toggleClass("hidden");
     
     countCov = ($("#"+targetRoot+"_1").children().length);
     if(numCov == countCov && numCov > 0) {
@@ -71,23 +88,47 @@ showCovariable = function(targetRoot, numCov) {
 }
 
 addCovariable = function(targetRoot) {
-    
 
     
+    // funcion que añade las secciones de una nueva covariable
     
     numCov = ($("#"+targetRoot+"_1").children().length);
+    
+    /*
+    el texto a añadir a cada subseccion de cada covariable
+    lo almaceno en el html en un div que esta siempre oculto display:none
+    para facilitar su edicion/actualización
+    replace("_xxx") sirve para actualizar los ids de la nueva covariable para
+    que no haya ids repetidos.
+    */
+    
+    
+    subsect_1_covariable_html = $("#covariables_1_html").html().replace(/_xxx/g,"_1_"+(numCov+1).toString());
+    subsect_2_covariable_html = $("#covariables_2_html").html().replace(/_xxx/g,"_2_"+(numCov+1).toString());
+    subsect_3_covariable_html = $("#covariables_3_html").html().replace(/_xxx/g,"_3_"+(numCov+1).toString());
+    subsect_4_covariable_html = $("#covariables_4_html").html().replace(/_xxx/g,"_4_"+(numCov+1).toString());
+    subsect_5_covariable_html = $("#covariables_5_html").html().replace(/_xxx/g,"_5_"+(numCov+1).toString());
+    
+    
+    
+    
     
     // actualizo contadores de navegador de covariables
     $("#countCovariable").html((numCov+1).toString());
     $("#punteroCovariable").html((numCov+1).toString());
 
     // añado subsecciones de la nueva covariable
-    $("#"+targetRoot+"_1").append("<div id='"+targetRoot+"_1_"+(numCov+1).toString()+"' class='row padded covariable-subsection'>Seccion para covariable "+(numCov+1).toString()+"</div>");
-    $("#"+targetRoot+"_2").append("<div id='"+targetRoot+"_2_"+(numCov+1).toString()+"' class='row padded covariable-subsection'>Seccion para covariable "+(numCov+1).toString()+"</div>");
-    $("#"+targetRoot+"_3").append("<div id='"+targetRoot+"_3_"+(numCov+1).toString()+"' class='row padded covariable-subsection'>Seccion para covariable "+(numCov+1).toString()+"</div>");
-    $("#"+targetRoot+"_4").append("<div id='"+targetRoot+"_4_"+(numCov+1).toString()+"' class='row padded covariable-subsection'>Seccion para covariable "+(numCov+1).toString()+"</div>");
+    $("#"+targetRoot+"_1").append("<div id='"+targetRoot+"_1_"+(numCov+1).toString()+"' class='row padded covariable-subsection'>"+subsect_1_covariable_html+"</div>");
+    $("#"+targetRoot+"_2").append("<div id='"+targetRoot+"_2_"+(numCov+1).toString()+"' class='row padded covariable-subsection'>"+subsect_2_covariable_html+"</div>");
+    $("#"+targetRoot+"_3").append("<div id='"+targetRoot+"_3_"+(numCov+1).toString()+"' class='row padded covariable-subsection'>"+subsect_3_covariable_html+"</div>");
+    $("#"+targetRoot+"_4").append("<div id='"+targetRoot+"_4_"+(numCov+1).toString()+"' class='row padded covariable-subsection'>"+subsect_4_covariable_html+"</div>");
+    $("#"+targetRoot+"_5").append("<div id='"+targetRoot+"_5_"+(numCov+1).toString()+"' class='row padded covariable-subsection'>"+subsect_5_covariable_html+"</div>");
+
+
     // muestro la nueva covariable
     showCovariable(targetRoot,numCov+1);
+    addBehaviourCovariableSelect(numCov+1);
+    $("#confusores_select").click(); // para que muestre la primera subsección
 }
 
 delCovariable = function(targetRoot, numCovToDel) {
@@ -105,20 +146,64 @@ delCovariable = function(targetRoot, numCovToDel) {
         $("#"+targetRoot+"_2_"+(numCovToDel).toString()).remove();
         $("#"+targetRoot+"_3_"+(numCovToDel).toString()).remove();
         $("#"+targetRoot+"_4_"+(numCovToDel).toString()).remove();
-
+        $("#"+targetRoot+"_5_"+(numCovToDel).toString()).remove();
         showCovariable(targetRoot, numCovToDel - 1);
     }
 };
 
-$('#add_diagnosis').click(function(e){
+addBehaviourCovariableSelect = function(numNewCov) {
+    select_id = "formTipoCovariable_1_"+(numNewCov).toString();
+    console.log(select_id);
+    $("#"+select_id).on("change",function(e){
+        e.preventDefault();
+        value_selected = $("#"+select_id.toString()+" :selected").attr("value");
+
+        covariableDefDiagnostico = $("#covariables_2_"+numNewCov.toString()).children(".covariable-diagnostico");
+        covariableDefFarmaco = $("#covariables_2_"+numNewCov.toString()).children(".covariable-farmaco");
+        covariableDefOtros = $("#covariables_2_"+numNewCov.toString()).children(".covariable-otros");
+        
+        covariableAvanDiagTrat = $("#covariables_3_"+numNewCov.toString()).children(".covariable-avanzadas-diagnostico-tratamiento");
+        covariableAvanOtros = $("#covariables_3_"+numNewCov.toString()).children(".covariable-avanzadas-otros");
+        
+
+        
+        hideElement(covariableDefDiagnostico);
+        hideElement(covariableDefFarmaco);
+        hideElement(covariableDefOtros);
+        hideElement(covariableAvanDiagTrat);
+        hideElement(covariableAvanOtros);
+        
+        switch (value_selected) {
+            case "diagnostico":
+                showElement(covariableDefDiagnostico);
+                showElement(covariableAvanDiagTrat);
+                break;
+            case "farmaco":
+                showElement(covariableDefFarmaco);
+                showElement(covariableAvanDiagTrat);
+                break;
+            default:
+                showElement(covariableDefOtros);
+                showElement(covariableAvanOtros);
+                break;
+                
+        }
+    });
+}
+
+
+//$('#add_diagnosis').click(function(e){
+$('#add_diagnosis').on("click", function(e){
     e.preventDefault();
     addDiagnosis(anadirDiagnosticoInput.value,$(document.body).data("botonesListaTarget"));
+    console.log($(document.body).data("botonesListaTarget"));
     anadirDiagnosticoInput.value = "";
     $("#addDiagnosisModal").modal('toggle');
 });
 
 
-$('#add_drug').click(function(e){
+//#$('#add_drug').click(function(e){
+$('#add_drug').on("click", function(e){    
     e.preventDefault();
     addDrug(anadirFarmacoInput.value,$(document.body).data("botonesListaTarget"));
     anadirFarmacoInput.value = "";
@@ -131,62 +216,24 @@ $("#miFormulario").on("click","button.borrar-elemento",function(e){
     $(this).parent().remove();
 });
 
-$(".botones-listas").on("click",function(e){
 
-    switch ($(this).attr("id")) {
-        case "addInclusionDiagnosisButton":
-            $(document.body).data("botonesListaTarget", "formInclusionDiagnosticosList" );
-            break;
-        case "addInclusionDrugButton":
-            $(document.body).data("botonesListaTarget", "formInclusionFarmacosList" );
-            break;    
-        case "addExclusionDiagnosisButton":
-            $(document.body).data("botonesListaTarget", "formExclusionDiagnosticosList" );
-            break;
-        case "addExclusionDrugButton":
-            $(document.body).data("botonesListaTarget", "formExclusionFarmacosList" );
-            break;
-        case "addEventoDiagnosisButton":
-            $(document.body).data("botonesListaTarget", "formEventoDiagnosticosList" );
-            break;
-        case "addEventoDrugButton":
-            $(document.body).data("botonesListaTarget", "formEventoDrugsList" );
-            break;
-        case "addEventoDiagnosisByDrugButton":
-            $(document.body).data("botonesListaTarget", "formEventoDiagnosisByDrugsList" );
-            break;
-        case "addExposicionDiagnosisButton":
-            $(document.body).data("botonesListaTarget", "formExposicionDiagnosticosList" );
-            break;
-        case "addExposicionDrugButton":
-            $(document.body).data("botonesListaTarget", "formExposicionDrugsList" );
-            break;
-        case "addExposicionDiagnosisByDrugButton":
-            $(document.body).data("botonesListaTarget", "formExposicionDiagnosisByDrugsList" );
-            break;
-        default:
-            $(document.body).data("botonesListaTarget", "no_target" );
-            break;
-    }
-});
 
 
 
 radioSwitcher = function (radioRoot, optionContainerId){
     
-    console.log($("."+radioRoot+".default").prop("checked", true));
-    console.log("."+radioRoot+".default");
+
     $("."+radioRoot).on("change", function(e){
         if($(this).is(':checked')) {
             $("#"+optionContainerId).toggleClass('hidden');
         }
     });
-    console.log("radioSwitcher: radioRoot: "+radioRoot+", optionContainerId: "+optionContainerId);
+
 }
 
 // initialize select and radio buttons
 $(document).ready(function() {
-    console.log("Document ready");
+
     if ($(".evento-diagnostico").hasClass("hidden")==true) {
         $(".evento-diagnostico").toggleClass("hidden")
     }
@@ -222,22 +269,28 @@ $(document).ready(function() {
     radioSwitcher("exposicionUsaDGP","exposicionUsaDGPOpciones");
     
     
-
+    radioSwitcher("emparejamiento","emparejamientoOpciones");
+    radioSwitcher("emparejamientoEdad","");
+    radioSwitcher("emparejamientoSexo","");
+    radioSwitcher("emparejamientoPeriodo","");
+    
+    radioSwitcher("incidenciaCalculo","incidenciaCalculoOpciones");
+    radioSwitcher("oddsRatioCalculo","oddsRatioCalculoOpciones");
+    radioSwitcher("subgrupoCalculo","subgrupoCalculoOpciones");
+    radioSwitcher("sensibilidadCalculo","sensibilidadCalculoOpciones");
     
 
     
     $("#addCovariable").click(function(e){
         e.preventDefault();
-        console.log("addCovariable");
         addCovariable("covariables");
     });
     $("#nextCovariable").click(function(e){
         e.preventDefault();
-        console.log("nextCovariable");
+
         numCov = ($("#covariables_1").children().length);
         pointerCov = parseInt($("#punteroCovariable").html());
-        console.log(numCov);
-        console.log(pointerCov);
+
         if(pointerCov < numCov) {
             newPointer = pointerCov + 1;
         } else {
@@ -248,11 +301,9 @@ $(document).ready(function() {
     });
     $("#prevCovariable").click(function(e){
         e.preventDefault();
-        console.log("prevCovariable");
+
         numCov = ($("#covariables_1").children().length);
         pointerCov = parseInt($("#punteroCovariable").html());
-        console.log(numCov);
-        console.log(pointerCov);
         if(pointerCov > 1) {
             newPointer = pointerCov - 1;
         } else {
@@ -268,9 +319,62 @@ $(document).ready(function() {
     $("#delCovariable").click(function(e){
         e.preventDefault();
         pointerCov = parseInt($("#punteroCovariable").html());
-        console.log("borrar "+pointerCov);
         delCovariable("covariables",pointerCov);
     });
+    
+    
+    //$(".botones-listas").on("click",function(e){
+    $("#miFormulario").on("click","button.botones-listas",function(e){
+        /*
+            almaceno localmente el div donde se van a añadir los elementos 
+            al pulsar un botón de "añadir diagnóstico" o "añadir fármaco"
+        */
+
+        switch ($(this).attr("id")) {
+            case "addInclusionDiagnosisButton":
+                $(document.body).data("botonesListaTarget", "formInclusionDiagnosticosList" );
+                break;
+            case "addInclusionDrugButton":
+                $(document.body).data("botonesListaTarget", "formInclusionFarmacosList" );
+                break;    
+            case "addExclusionDiagnosisButton":
+                $(document.body).data("botonesListaTarget", "formExclusionDiagnosticosList" );
+                break;
+            case "addExclusionDrugButton":
+                $(document.body).data("botonesListaTarget", "formExclusionFarmacosList" );
+                break;
+            case "addEventoDiagnosisButton":
+                $(document.body).data("botonesListaTarget", "formEventoDiagnosticosList" );
+                break;
+            case "addEventoDrugButton":
+                $(document.body).data("botonesListaTarget", "formEventoDrugsList" );
+                break;
+            case "addEventoDiagnosisByDrugButton":
+                $(document.body).data("botonesListaTarget", "formEventoDiagnosisByDrugsList" );
+                break;
+            case "addExposicionDiagnosisButton":
+                $(document.body).data("botonesListaTarget", "formExposicionDiagnosticosList" );
+                break;
+            case "addExposicionDrugButton":
+                $(document.body).data("botonesListaTarget", "formExposicionDrugsList" );
+                break;
+            case "addExposicionDiagnosisByDrugButton":
+                $(document.body).data("botonesListaTarget", "formExposicionDiagnosisByDrugsList" );
+                break;
+            default:
+                $(document.body).data("botonesListaTarget", "no_target" );
+                break;
+        }
+    });
+    
+    
+    /* help buttons 
+    $(".help-icon").on("click",function(e){
+        console.log($(this).attr("help-target"));
+    });
+    */
+    
+    
 
 });
 // end of initialize select and radio buttons
